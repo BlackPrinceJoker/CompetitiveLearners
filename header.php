dbe9c78fdcdc84682ec0ff69a304daee453149a5
BlackPrinceJoker <90432423+BlackPrinceJoker@users.noreply.github.com>
2022-07-29 22:19:22 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 	<html>
 		<head>
-			<title>Escambia Handyman Services LLC</title>
+			<title>Competitive Learners</title>
 			<meta charset="utf-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1">
 			<link rel="icon" href="icon5.png" type="image/png">
@@ -9,41 +9,25 @@
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 			<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 		</head>
-		
-		<style>
-			.orange {
-				background-color: #ff4400;
-			}
-			.grey {
-				background-color: #262626;
-				color: white;
-			}
-			.white {
-				background-color: white;
-			}
-			.pt-6 {
-				margin-top: 200px;
-			}
-			
-		</style>
 		
 		<body>
 			<nav class="navbar navbar-expand-sm navbar-dark grey">
-				<a class="navbar-brand" href="#">Escambia Handyman</a>
+				<a class="navbar-brand" href="index.php">Competitive Learners</a>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 				<div class="collapse navbar-collapse" id="collapsibleNavbar">
 					<ul class="navbar-nav">
 						<li class="nav-item">
-							<a class="nav-link" href="#services">Services</a>
+							<a class="nav-link" href="math.php">Math</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" href="#about">About</a>
+							<a class="nav-link" href="#other stuff">other stuff</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" href="#contact">Contact</a>
+							<a class="nav-link" href="#more stuff">more stuff</a>
 						</li>
 					</ul>
 				</div>
